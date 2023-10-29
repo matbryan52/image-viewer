bace5ed18257916a7506cbf500fca7aaf2d503cd
Matthew Bryan <matthewbryan52@gmail.com>
2023-10-29 09:06:36 +0100

    Update app

--- src/image_viewer/app.py
+++ src/image_viewer/app.py
@@ -1,6 +1,29 @@
+import numpy as np
 import panel as pn
+from bokeh.plotting import figure
+
+from libertem_ui.display.image_db import BokehImage
+from libertem_ui.live_plot import adapt_figure
 
 
-pn.widgets.StaticText(
-    value="Hello world"
-).servable()
+def viewer():
+    header_md = pn.pane.Markdown(object="""
+# Image viewer""")
+
+    shape = (480, 640)
+    fig = figure()
+    img = (
+        BokehImage
+        .new()
+        .from_numpy(np.random.uniform(size=shape).astype(np.float32))
+    )
+    img.on(fig)
+    adapt_figure(fig, shape, maxdim=600)
+
+    return pn.Column(
+        header_md,
+        pn.pane.Bokeh(fig),
+    )
+
+
+viewer().servable()
