29b42ad35d420b45ff88c10a58f24c61422fa11c
Matthew Bryan <78845903+matbryan52@users.noreply.github.com>
2023-11-02 15:58:59 +0100

    Use upstream ApertureFigure

--- src/image_viewer/components.py
+++ src/image_viewer/components.py
@@ -8,9 +8,6 @@ from skimage.io import imread
 from rsciio import IO_PLUGINS
 import numpy as np
 
-from libertem_ui.display.image_db import BokehImage
-from libertem_ui.live_plot import adapt_figure, AperturePlot
-
 
 class LoadException(RuntimeError):
     ...
@@ -58,10 +55,11 @@ def load_local(url_hash) -> tuple[np.ndarray, dict]:
     path = pathlib.Path(path).expanduser().resolve()
     try:
         array = load_rsciio(path)
+    except LoadException as e:
+        array = imread(path, as_gray=True)
     except Exception as e:
         print(e)
         raise
-    # array = imread(path, as_gray=True)
     return array, {'path': str(path), 'title': path.name}
 
 
@@ -74,62 +72,3 @@ def load_url(url_hash) -> tuple[np.ndarray, dict]:
     components = urllib.parse.urlsplit(url)
     title = components.path.split('/')[-1]
     return array, {'url': url, 'title': title}
-
-
-class ApertureFigure:
-    def __init__(self, fig, im):
-        """
-        Minimal version of AperturePlot from LiberTEM-panel-ui
-        without components necessary to be compatible with the LivePlot2D
-        interface. With some refactoring upstream this class would
-        no longer be necessary. It mostly adds the toolbar (hidden) and
-        the methods to add the control panel / hover text
-        """
-        self._fig = fig
-        self._im = im
-        self._pane = pn.pane.Bokeh(fig)
-        self._toolbar = pn.Row(
-            height=1,
-            margin=(0, 0)
-        )
-        self._layout = pn.Column(
-            self._toolbar,
-            self._pane,
-            margin=(0, 0),
-        )
-
-    @classmethod
-    def new(cls, array: np.ndarray, title=None):
-        fig = figure()
-        if title is not None:
-            fig.title.text = title
-        img = (
-            BokehImage
-            .new()
-            .from_numpy(array)
-        )
-        img.on(fig)
-        img.enable_downsampling()
-        adapt_figure(fig, array.shape, maxdim=600)
-        return ApertureFigure(fig, img)
-
-    @property
-    def fig(self) -> figure | None:
-        return self._fig
-
-    @property
-    def im(self) -> BokehImage | None:
-        return self._im
-
-    @property
-    def layout(self) -> pn.Column:
-        return self._layout
-
-    def add_hover_position_text(self):
-        return AperturePlot.add_hover_position_text(self)
-
-    def add_control_panel(
-        self,
-        name: str = 'Image Controls',
-    ):
-        return AperturePlot.add_control_panel(self, name=name)
