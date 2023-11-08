e114300cbd7e86a36ee0ddc960be36506f9552fe
Matthew Bryan <matthewbryan52@gmail.com>
2023-11-08 11:00:17 +0100

    Simplify token path handling

--- src/image_viewer/cli.py
+++ src/image_viewer/cli.py
@@ -14,15 +14,17 @@ from . import APP_FILE
 def serve(port: int, token_path: str | None):
     token = None
     if token_path is not None:
-        try:
-            token_path = pathlib.Path(token_path)
-        except (ValueError, TypeError):
-            pass
-        if token_path.is_file():
-            token = token_path.open('r').read()
+        # Don't handle any errors so that an
+        # invalid token path stops the launch
+        token_path = pathlib.Path(token_path)
+        with token_path.open('r') as fp:
+            token = fp.read()
+            print('Using token auth')
+    if token is None:
+        print('No token auth')
     pn.state.as_cached(
         key='api_token',
-        fn=lambda: token,
+        fn=(lambda: token),
     )
     pn.serve(
         APP_FILE,
